1d6149708da18d3da022cadfc272c0a912af5e54
Daniel Kogler <danielk373@gmail.com>
2020-08-10 11:34:16 -0500

    Create setup.sh
    Runtime is not included in this

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+read -p "Enter Your GitHub Username: "  username
+read -s -p "Enter Your GitHub Password: "  password
+
+git clone https://${username}:${password}@github.com/operationspark/circularity
+git clone https://github.com/operationspark/platformer
+git clone https://github.com/operationspark/bouncing-box
+
+cd platformer
+rm -rf .git* .master
+bower install
+cd ../bouncing-box
+rm -rf .git* .master
+cd ../circularity
+rm -rf .git* .master
+bower install
+
+
