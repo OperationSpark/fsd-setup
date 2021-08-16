171b3e7fe33bb025448c1ad281315ec16efea1a0
Daniel Kogler <danielk373@gmail.com>
2021-08-16 18:00:27 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-read -p "Enter Your GitHub Username: "  username
-read -s -p "Enter Your GitHub Password: "  password
 
 printf "\n"
 
@@ -11,9 +9,9 @@ fi
 mkdir projects
 cd projects
 
-git clone https://${username}:${password}@github.com/operationspark/circularity
-git clone https://${username}:${password}@github.com/operationspark/platformer
-git clone https://${username}:${password}@github.com/operationspark/bouncing-box
+git clone https://github.com/operationspark/circularity
+git clone https://github.com/operationspark/platformer
+git clone https://github.com/operationspark/bouncing-box
 
 if ! test -e platformer || ! test -e bouncing-box || ! test -e circularity; then
     printf "\nFAILURE: Some projects could not be accessed on GitHub. Please run the script again."
@@ -34,8 +32,8 @@ else
 
     #test bower_components
     if ! test -e bower_components; then
-        printf "\nFAILURE: bower install failed. Please run 'npm install -g opspark', then run this script again."
+        printf "\nFAILURE: bower install failed. Please run 'npm install -g opspark', then run this script again.\n"
     else
-        printf "\nSUCCESS: All projects succesfully installed."
+        printf "\nSUCCESS: All projects succesfully installed.\n"
     fi
 fi
