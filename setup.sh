7597012a5569b33563aba164814f418414b73fba
Daniel Kogler <danielk373@gmail.com>
2022-10-04 21:49:17 -0500

    script now installs READMEs to a 'project-instructions' folder

--- setup.sh
+++ setup.sh
@@ -9,13 +9,26 @@ fi
 mkdir projects
 cd projects
 
+git clone https://github.com/operationspark/first-website-hs
+git clone https://github.com/operationspark/portfolio-hs
 git clone https://github.com/operationspark/circularity
 git clone https://github.com/operationspark/platformer
 git clone https://github.com/operationspark/bouncing-box
 
-if ! test -e platformer || ! test -e bouncing-box || ! test -e circularity; then
+if ! test -e first-website-hs || ! test -e portfolio-hs || ! test -e platformer || ! test -e bouncing-box || ! test -e circularity; then
     printf "\nFAILURE: Some projects could not be accessed on GitHub. Please run the script again."
 else
+    #READMEs
+    mkdir ../project-instructions
+    cp first-website-hs/README.md ../project-instructions/first-website.md
+    cp portfolio-hs/README.md ../project-instructions/portfolio.md
+    cp platformer/README.md ../project-instructions/platformer.md
+    cp bouncing-box/README.md ../project-instructions/bouncing-box.md
+    cp circularity/README.md ../project-instructions/circularity.md
+
+    #Cleanup first website and portfolio
+    rm -rf first-website-hs portfolio-hs
+
     #platformer
     cd platformer
     rm -rf .git* .master
